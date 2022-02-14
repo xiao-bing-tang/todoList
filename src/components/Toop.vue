35b007030cc5bdd78a8a63615ebdf761fe2504a6
xiao-bing-tang <2546416155@qq.com>
2022-02-14 11:54:49 +0800

    初始化仓库

---
+++ src/components/Toop.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入你的任务名称，按回车键确认" v-model="title" @keyup.enter="add"/>
+  </div>
+</template>
+
+<script>
+  import {nanoid} from 'nanoid'
+  export default {
+    name: 'Header',
+    data(){
+      return{
+        title:''
+      }
+    },
+    props:['addTodo'],
+    methods:{
+      add(){
+        if(!this.title.trim()) return alert('请输入内容')
+          //将输入信息包装为todo对象
+          const todoObj = {id:nanoid(),title:this.title,done:false}
+          this.addTodo(todoObj)
+          this.title = ''
+        
+          
+        
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    /*header*/
+  .todo-header input {
+    width: 560px;
+    height: 28px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 4px 7px;
+  }
+
+  .todo-header input:focus {
+    outline: none;
+    border-color: rgba(82, 168, 236, 0.8);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+  }
+
+</style>
